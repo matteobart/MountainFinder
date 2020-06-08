5b00a661b34c0db7cb49479e4aa5f9bb9a498c2a
Matteo Bartalini <MatteoBartalini1@gmail.com>
2020-06-07 20:04:07 -0400

    Initial Commit

---
+++ Mountain Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mountain Finder
+//
+//  Created by Matteo Bart on 6/7/20.
+//  Copyright © 2020 Matteo Bart. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mountain FinderTests/Mountain_FinderTests.swift
@@ -0,0 +1,34 @@
+//
+//  Mountain_FinderTests.swift
+//  Mountain FinderTests
+//
+//  Created by Matteo Bart on 6/7/20.
+//  Copyright © 2020 Matteo Bart. All rights reserved.
+//
+
+import XCTest
+@testable import Mountain_Finder
+
+class Mountain_FinderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
